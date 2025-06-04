8da26f17a172d01e1aaefae505d41b21355407e7
Kevin Jose Mendoza Tobar <mt25001@esfe.agape.edu.sv>
2025-06-04 16:45:23 -0600

    cambio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hola mis amores ...");
         System.out.println("Xd ...");
+        System.out.println("estamos en el prime");
     }
 }
