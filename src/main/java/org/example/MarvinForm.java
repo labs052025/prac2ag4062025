ab3fccb245fd6ce5984e92ac34b9cc93f58353db
Marvin Antonio Barrera Trigueros <antoniobtrigueros@gmail.com>
2025-06-04 16:36:12 -0600

    modifique mi formulario

---
+++ src/main/java/org/example/MarvinForm.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+public class MarvinForm {
+    private JTextField textField1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JTextField textField2;
+}
